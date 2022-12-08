ec5544ed6880b89ed08e08160c3d2c700836fbcc
preesee <why123_hua@163.com>
2022-12-08 15:35:35 +0800

    test for main on loop condition(rename)

--- src/test_main.py
+++ src/test_main.py
@@ -5,4 +5,5 @@ from src.main import is_pick_unlabeled_data
 data =pd.read_csv('/data/huawei/Semi-SSL/CUB200/test_loop.csv')
 
 confidence_unlabeled=0.95
-is_pick_unlabeled_data(data,confidence_unlabeled)
+ret_val=is_pick_unlabeled_data(data,confidence_unlabeled)
+print(ret_val)
