b538523ce3ebdff16ec4542fba60a0a647d0c4fd
GANPerf <898226219@qq.com>
2022-12-19 23:50:38 +1030

    adjust main.py all parameters used in function

--- run.sh
+++ run.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+export PYTHONPATH=$(pwd)
+
 python src/main.py  --root ./Aircraft --batch_size 24 --logdir vis/ --gpu_id 0 --queue_size 32 --projector_dim 1024 --backbone resnet50 --label_ratio 15 --pretrained
 python src/main.py  --root ./Aircraft --batch_size 24 --logdir vis/ --gpu_id 1 --queue_size 32 --projector_dim 1024 --backbone resnet50 --label_ratio 30 --pretrained
 python src/main.py  --root ./Aircraft --batch_size 24 --logdir vis/ --gpu_id 2 --queue_size 32 --projector_dim 1024 --backbone resnet50 --label_ratio 50 --pretrained
